2d40406c5e6217e8f76ba7689edaee3511a9563f
Christophe Massolin <christophe.massolin@makemepulse.com>
2016-03-03 17:05:48 +0100

    Add POSTCSS support

---
+++ workflow/tasks/postcss.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+'use strict';
+
+const Print   = require('./../lib/Print')
+const exec    = require('child_process').exec
+const fs      = require('fs')
+
+const POSTCSS = function(options) {
+
+  const input  = options.i;
+  const output = options.o;
+
+  var params = [`${__dirname}/../../node_modules/.bin/postcss`];
+  if (options.sourcemaps) params.push("--map");
+  if (options.watch) params.push("--watch")
+
+  params.push(`--use ${__dirname}/../../node_modules/autoprefixer`)
+
+  params.push(`--output ${output}`)
+  params.push(input)
+
+  const cli = exec(params.join(' '))
+
+  cli.stdout.setEncoding('utf-8')
+  cli.stdout.on('data', function(data) {
+    data = Print.clean(data.toString('utf-8'))
+    Print.log(`[POSTCSS] ${data}`, true, 'magenta')
+  });
+
+  cli.stderr.on('data', function(data) {
+    data = Print.clean(data.toString('utf-8'))
+    Print.log(`[POSTCSS] ${data}`, true, 'red')
+  });
+
+  cli.on('close', function(code){
+    Print.log(`[POSTCSS] child process exited with code ${code}`, true, 'magenta')
+  });
+
+}
+
+module.exports = POSTCSS
