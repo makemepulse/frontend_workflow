8ebd5e3783ff4dfa21dd60cea9965bd0c58d8ecf
Christophe Massolin <christophe.massolin@makemepulse.com>
2016-03-17 17:56:19 +0100

    Fix filename from template tasks

--- workflow/lib/template.js
+++ workflow/lib/template.js
@@ -8,6 +8,13 @@ const Print = require('./print')
 
 const Template = (function(){
 
+  /**
+   *
+   * @type Object
+   * @private
+   *
+   * Regex used to detect the file type
+   */
   const _regex = {
     html: /html/i,
     js: /(js|es)/i,
@@ -15,52 +22,85 @@ const Template = (function(){
     sass: /(sass)/i
   }
 
+  /**
+   *
+   * @type {string}
+   * @private
+   *
+   * Template path
+   */
   const _templatePath = [
     __dirname, '..', 'templates'
   ].join('/')
 
+
+  /**
+   *
+   * @param options
+   * @private
+   *
+   * Create template with options
+   *
+   */
   const _create = function(options) {
 
     let config   = options.config
     const templateName = options._[0] === 'template' ? options._[1] : options._[0]
 
+    // Check template exists
     if (!config[templateName]) {
       Print.log(`The "${templateName}" template do not have any configuration`, true, 'yellow')
       return
     }
 
+    // Get template configuration
     config = config[templateName]
     const templatePaths = []
 
+    // Get command line parameters
+    let params = { name: options._[0] === 'template' ? options._[2] : options._[1] }
+    if (typeof params.name !== 'string') params = params.name
+
+    // Build a manifest from template files
     for (let file, output, pth, i = 0; i < config.files.length; i++) {
       file = config.files[i]
 
       if (path.dirname(file) === '.') {
         for (let key in _regex) {
           if (file.match(_regex[key])) {
+
+            // Detect a file
             if (path.extname(file).length > 0) {
               pth = [_templatePath, key, file]
-            } else {
+            }
+
+            // Detect an extension
+            else {
               pth = [_templatePath, key, templateName + '.' + file]
             }
+
             break
           }
         }
-      } else {
+      }
+
+      // Detect a path
+      else {
         pth = [_templatePath, file]
       }
 
       pth = pth.join('/')
-      output = config.output ? config.output + path.extname(pth) : templateName + path.extname(pth)
+
+      // Override the output name if the config.ouput exists
+      output = config.output ? config.output + path.extname(pth) : params.name + path.extname(pth)
+
       templatePaths.push({
         inputFile: pth,
         outputFile: output
       })
     }
 
-    let params = { name: options._[0] === 'template' ? options._[2] : options._[1] }
-    if (typeof params.name === 'object') params = params.name
-
+    // Generate files from templates
     for (let j = 0; j < templatePaths.length; j++) {
       const template = fs.readFileSync(templatePaths[j].inputFile).toString('utf8')
       const output   = m.render(template, params)
