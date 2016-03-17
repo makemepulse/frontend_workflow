a53769ac7444568e0d1dce2105ecfc5ec1d27d74
Christophe Massolin <christophe.massolin@makemepulse.com>
2016-03-17 17:56:35 +0100

    Add comments to config.js

--- workflow/lib/config.js
+++ workflow/lib/config.js
@@ -8,8 +8,8 @@ const yaml = require('js-yaml')
 const Config = function(options){
 
   let content = fs.readFileSync(`${__dirname}/../config.yml`).toString('utf-8')
-  content     = m.render(content, yaml.load(content))
-  content     = m.render(content, yaml.load(content))
+  content     = m.render(content, yaml.load(content)) // Render yaml to yaml object
+  content     = m.render(content, yaml.load(content)) // Render again with yaml object in parameters
   return yaml.load(content)
 
 }
