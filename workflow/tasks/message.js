f90620191770d262a84d5668d1bcfaf7e279d1a9
Christophe Massolin <christophe.massolin@gmail.com>
2016-09-10 18:49:40 +0200

    Test new starter

---
+++ workflow/tasks/message.js
@@ -0,0 +1,26 @@
+'use strict'
+
+namespace('message', function() {
+
+  task('hello', { async: true }, function( complete ) {
+    console.log('Hello World')
+    setTimeout(function() {
+      complete()
+    })
+  })
+
+  task('surprise', function() {
+    console.log('Surprise Mother Fucka')
+  })
+
+  task('group', ['message:hello', 'message:surprise'], function() {
+    console.log('All message sent!')
+  })
+
+  command('plop', 'echo plop')
+
+  task('all', [ 'message:hello', 'message:surprise', 'message:plop' ], { preReqSequence: 'serie' }, function() {
+    console.log('Everything executed with success')
+  })
+
+})
